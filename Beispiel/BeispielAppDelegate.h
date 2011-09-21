d71b769c5ddfc5aaa1f30245b2ccc770d97f555f
Christian Schuldes <chris@chris.local>
2011-09-21 20:27:24 +0200

    Initial Commit

---
+++ Beispiel/BeispielAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BeispielAppDelegate.h
+//  Beispiel
+//
+//  Created by Schuldes Christian on 21.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeispielAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
